46ddaf76e9371b65f24b3c49932bec2120c4b197
aokihu <aokihu@gmail.com>
2024-10-13 00:02:55 +0800

    更新相关头文件

---
+++ includes/cli_parser.h
@@ -0,0 +1,42 @@
+#ifndef CLI_PARSER_H
+#define CLI_PARSER_H
+
+#include <glib.h>
+
+typedef struct
+{
+  gboolean debug_mode;
+  gchar *config_file_path;
+  gchar *alsa_device;
+  gchar *device_name;
+  gboolean show_version;
+} CliOptions;
+
+/**
+ * @brief 解析命令行参数
+ *
+ * @param argc 参数数量
+ * @param argv 参数数组
+ * @param options 用于存储解析结果的CliOptions结构体指针
+ * @return gboolean 解析是否成功
+ */
+gboolean parse_cli_options(int argc, char *argv[], CliOptions *options);
+
+/**
+ * @brief 打印帮助信息
+ */
+void print_help(void);
+
+/**
+ * @brief 打印版本信息
+ */
+void print_version(void);
+
+/**
+ * @brief 释放CliOptions结构体中的动态分配内存
+ *
+ * @param options 需要清理的CliOptions结构体指针
+ */
+void free_cli_options(CliOptions *options);
+
+#endif /* CLI_PARSER_H */
---
+++ includes/upnp_service.h
@@ -0,0 +1,56 @@
+#ifndef UPNP_SERVICE_H
+#define UPNP_SERVICE_H
+
+#include <glib.h>
+#include "app_context.h"
+
+// UPnP 上下文结构体
+typedef struct UPnPContext UPnPContext;
+
+/**
+ * @brief 初始化 UPnP 服务
+ *
+ * @param app_context 应用程序上下文
+ * @return UPnPContext* UPnP 上下文指针，如果初始化失败则返回 NULL
+ */
+UPnPContext *upnp_service_init(AppContext *app_context);
+
+/**
+ * @brief 清理 UPnP 服务
+ *
+ * @param upnp_context UPnP 上下文指针
+ */
+void upnp_service_cleanup(UPnPContext *upnp_context);
+
+/**
+ * @brief 启动 UPnP 设备
+ *
+ * @param upnp_context UPnP 上下文指针
+ * @return gboolean 启动是否成功
+ */
+gboolean upnp_service_start(UPnPContext *upnp_context);
+
+/**
+ * @brief 停止 UPnP 设备
+ *
+ * @param upnp_context UPnP 上下文指针
+ */
+void upnp_service_stop(UPnPContext *upnp_context);
+
+/**
+ * @brief 设置 UPnP 设备的友好名称
+ *
+ * @param upnp_context UPnP 上下文指针
+ * @param friendly_name 友好名称
+ */
+void upnp_service_set_friendly_name(UPnPContext *upnp_context, const gchar *friendly_name);
+
+/**
+ * @brief 获取 UPnP 设备的友好名称
+ *
+ * @param upnp_context UPnP 上下文指针
+ * @return const gchar* 友好名称
+ */
+const gchar *upnp_service_get_friendly_name(UPnPContext *upnp_context);
+
+#endif /* UPNP_SERVICE_H */
