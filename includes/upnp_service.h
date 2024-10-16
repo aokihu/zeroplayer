c62c4a98d1b8ca6f2d410145f47e5a2ff20420b9
aokihu <aokihu@gmail.com>
2024-10-16 23:17:20 +0800

    新增upnp_device_description_generate方法生成设备描述文件

--- includes/upnp_service.h
+++ includes/upnp_service.h
@@ -7,6 +7,7 @@
 // UPnP 上下文结构体
 typedef struct
 {
+  GUPnPContext *upnp_context;    /**< GUPnP 上下文对象 */
   GUPnPRootDevice *rootDevice;   /**< GUPnP 根设备对象 */
   GUPnPDeviceInfo *deviceInfo;   /**< GUPnP 设备信息对象 */
   gchar *friendlyName;           /**< 设备的友好名称 */
@@ -31,6 +32,17 @@ UPnPContext *upnp_service_init(struct AppContext *app_context);
  * @param upnp_context UPnP 上下文指针
  */
 void upnp_service_cleanup(UPnPContext *upnp_context);
+
+/**
+ * @brief 生成设备描述XML
+ *
+ * @param friendlyName 友好名称
+ * @param manufacturer 制造商
+ * @param modelName 模型名称
+ * @param udn UDN
+ * @return 设备描述XML
+ */
+gchar *upnp_device_description_generate(const gchar *friendlyName, const gchar *manufacturer, const gchar *modelName, const gchar *udn);
 
 /**
  * @brief 启动 UPnP 设备
