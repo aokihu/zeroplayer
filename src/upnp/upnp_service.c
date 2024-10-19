804b68026b5bf0d7c48a48636f9b4e3b11907ae7
aokihu <aokihu@gmail.com>
2024-10-19 21:12:57 +0800

    增加gupnp_context_get_host_ip静态方法获取上下文对象IP地址，并且实现RootDevice发现功能

--- src/upnp/upnp_service.c
+++ src/upnp/upnp_service.c
@@ -7,6 +7,17 @@
 #include <libgupnp/gupnp-service-info.h>
 
 /**
+ * @brief 获取UPnP上下文的IP地址
+ *
+ * @param context GUPnPContext指针
+ * @return const char* IP地址字符串
+ */
+static const char *gupnp_context_get_host_ip(GUPnPContext *context)
+{
+  return gssdp_client_get_host_ip(GSSDP_CLIENT(context));
+}
+
+/**
  * @brief Initialize UPnP service
  *
  * @param app_context Application context pointer
@@ -16,9 +27,9 @@ void upnp_service_init(struct AppContext *app_context)
 {
   g_print("Initializing UPnP service By Manager\n");
 
-  GUPnPContextManager *manager = gupnp_context_manager_create(5050);
-  g_signal_connect(manager, "context-available", G_CALLBACK(upnp_on_context_available), NULL);
-  g_signal_connect(manager, "context-unavailable", G_CALLBACK(upnp_on_context_unavailable), NULL);
+  GUPnPContextManager *manager = gupnp_context_manager_create(0);
+  g_signal_connect(manager, "context-available", G_CALLBACK(upnp_on_context_available), app_context);
+  g_signal_connect(manager, "context-unavailable", G_CALLBACK(upnp_on_context_unavailable), app_context);
   g_print("UPnP context manager created\n");
 }
 
@@ -53,15 +64,31 @@ gboolean upnp_service_start(UPnPContext *upnp_context)
  *
  * @param manager GUPnPContextManager指针
  * @param context GUPnPContext指针
- * @param user_data 用户数据
+ * @param user_data 用户数据, 指向AppContext结构体指针
  */
 void upnp_on_context_available(GUPnPContextManager *manager,
                                GUPnPContext *context,
                                gpointer user_data)
 {
-  g_print("UPnP context available\n");
+  const char *ip = gupnp_context_get_host_ip(context);
+  g_print("UPnP context available IP: %s\n", ip);
+
+  GError *error = NULL;
+
+  GUPnPRootDevice *rd = gupnp_root_device_new(context, "description.xml", "./XML", &error);
+  gupnp_context_manager_manage_root_device(manager, rd);
+  gupnp_root_device_set_available(rd, TRUE);
+
+  g_object_unref(rd);
 }
 
+/**
+ * @brief 当UPnP上下文不可用时调用
+ *
+ * @param manager GUPnPContextManager指针
+ * @param context GUPnPContext指针
+ * @param user_data 用户数据, 指向AppContext结构体指针
+ */
 void upnp_on_context_unavailable(GUPnPContextManager *manager,
                                  GUPnPContext *context,
                                  gpointer user_data)
